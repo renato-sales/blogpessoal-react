1dfb64f242098886d0acd4b46edbba2aa278f903
Renato Sales <renatosalespaula@gmail.com>
2026-02-19 15:39:30 -0300

    implementação do CRUD dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import FormTema from "./components/tema/formtema/FormTema";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
 import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
@@ -17,6 +19,9 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -17,3 +17,32 @@ export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
+
+export const cadastrar = async (
+  url: string,
+  dados: object,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.post(url, dados, header);
+  setDados(resposta.data);
+};
+
+export const atualizar = async (
+  url: string,
+  dados: object,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.put(url, dados, header);
+  setDados(resposta.data);
+};
