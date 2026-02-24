b2edcb565591c5d69421aa9e9a077a5d95fa2941
Renato Sales <renatosalespaula@gmail.com>
2026-02-24 20:10:44 -0300

    Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-pessoal-kq0y.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const cadastrarUsuario = async (
